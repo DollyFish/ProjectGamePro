49711e7741a5d2f227dfd47ff9d4dd60543af6a2
6230403890-oop-labs <69180953+6230403890-oop-labs@users.noreply.github.com>
2021-11-24 00:33:47 +0700

    HUD update 1

---
+++ TestProject/Assets/Scripts/ProgressBarGrade.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[ExecuteInEditMode()]
+public class ProgressBarGrade : MonoBehaviour
+{
+    public int maximum;
+    public int current;
+    public collectitem collectitemScript;
+
+    public Image mask;  
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetCurrentFill();
+    }
+    void GetCurrentFill(){
+        
+        float fillAmount = (float)collectitemScript.daypoint/ (float)maximum;
+        mask.fillAmount = fillAmount;
+    }
+}
